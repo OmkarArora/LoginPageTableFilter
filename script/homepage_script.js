acb9d8b6fa594dad45d627deb21641168968b7e4
OmkarArora <aroraomkar@gmail.com>
2020-02-01 14:56:16 +0530

    Pop up added

--- script/homepage_script.js
+++ script/homepage_script.js
@@ -5,6 +5,8 @@ let countryArray = [];
 const cityFilter = document.getElementById("cityFilter");
 const stateFilter = document.getElementById("stateFilter");
 const countryFilter = document.getElementById("countryFilter");
+const modal = document.getElementById("modalPopUp");
+const modalSpan = document.getElementsByClassName("close-modal")[0];
 
 const setUpPage = () => {
   fetch("../data/MOCK_DATA.json")
@@ -65,7 +67,7 @@ const searchTable = () => {
   const cityFilterValue = cityFilter.value.toUpperCase();
   const stateFilterValue = stateFilter.value.toUpperCase();
   const countryFilterValue = countryFilter.value.toUpperCase();
-  let filter = document.getElementById("searchButton").value.toUpperCase();
+  //let filter = document.getElementById("searchButton").value.toUpperCase();
   let tableElement = document.querySelector(".info-table");
   let tableRow = tableElement.getElementsByTagName("tr");
 
@@ -73,6 +75,8 @@ const searchTable = () => {
   for (let i = 1; i < tableRow.length; i++) {
     tableRow[i].style.display = "";
   }
+  let dataCount = tableRow.length;
+  let hideCount = 0;
 
   for (let i = 1; i < tableRow.length; i++) {
     let city = tableRow[i]
@@ -91,6 +95,7 @@ const searchTable = () => {
       if (city.indexOf(cityFilterValue) > -1) tableRow[i].style.display = "";
       else {
         tableRow[i].style.display = "none";
+        hideCount++;
         cityFlag = true;
       }
     }
@@ -98,13 +103,35 @@ const searchTable = () => {
       if (state.indexOf(stateFilterValue) > -1) tableRow[i].style.display = "";
       else {
         tableRow[i].style.display = "none";
+        hideCount++;
         stateFlag = true;
       }
     }
     if (countryFilterValue !== "ALL" && !cityFlag && !stateFlag) {
       if (country.indexOf(countryFilterValue) > -1)
         tableRow[i].style.display = "";
-      else tableRow[i].style.display = "none";
+      else {
+        tableRow[i].style.display = "none";
+        hideCount++;
+      }
     }
+  }
+  console.log(hideCount);
+  console.log(dataCount);
+  if (hideCount === dataCount - 1) {
+    displayPopUp();
+  }
+};
+
+const displayPopUp = () => {
+  modal.style.display = "block";
+};
+modalSpan.onclick = function() {
+  modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 };
